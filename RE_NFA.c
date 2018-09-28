f31ac70cd01122c9b44b383e7bea198c35d97fd3
Ben Kuehnert <bkuehnert@gmail.com>
2018-09-28 17:18:40 -0400

    fixed bounds errors

--- RE_NFA.c
+++ RE_NFA.c
@@ -14,15 +14,17 @@ NFA create_Concat(NFA a, NFA b)
 	//malloc out a new trans table
 	int new_size = a->num_states+b->num_states;
 	NFA ret = new_NFA(new_size, "");
-	NFA_add_transition(ret, a->num_states, 0, a->num_states+1);
+	NFA_add_transition(ret, a->num_states-1, 0, a->num_states);
 	for(int i=0; i<new_size; i++)
 	{
 		for(int j=0; j<128; j++)
 		{
 			if(i<a->num_states)
 				Set_union(ret->trans_table[i][j], a->trans_table[i][j]);
-			if(i>=a->num_states && i<b->num_states)
+			if(i>=a->num_states){
+				Set_incr(b->trans_table[i-a->num_states][j], a->num_states);
 				Set_union(ret->trans_table[i][j], b->trans_table[i-a->num_states][j]);
+			}
 		}		
 	}
 	NFA_set_accepting(ret, new_size-1, true);
@@ -36,15 +38,19 @@ NFA create_Union(NFA a, NFA b)
 	int new_size = a->num_states + b->num_states + 2;
 	NFA ret = new_NFA(new_size, "");
 	NFA_add_transition(ret, 0, 0, 1);
-	NFA_add_transition(ret, 0, 0, a->num_states+2);
-	NFA_add_transition(ret, a->num_states+1, 0, a->num_states+b->num_states+2);
-	NFA_add_transition(ret, a->num_states+b->num_states+1, 9, a->num_states+b->num_states+2);
+	NFA_add_transition(ret, 0, 0, a->num_states+1);
+	NFA_add_transition(ret, a->num_states, 0, a->num_states+b->num_states+1);
+	NFA_add_transition(ret, a->num_states+b->num_states, 0, a->num_states+b->num_states+1);
 	for (int i = 0; i < new_size;i++) {
 		for (int j = 0; j<128; j++) {
-			if(i > 0 && i < a->num_states+1)
+			if(i > 0 && i < a->num_states+1){
+				Set_incr(a->trans_table[i-1][j],1);
 				Set_union(ret->trans_table[i][j], a->trans_table[i-1][j]);
-			if(i>a->num_states && i<a->num_states+b->num_states+1)
+			}
+			if(i>a->num_states && i<a->num_states+b->num_states+1){
+				Set_incr(b->trans_table[i-a->num_states-1][j],a->num_states+1);
 				Set_union(ret->trans_table[i][j], b->trans_table[i-a->num_states-1][j]);
+			}
 		}
 	}
 	NFA_set_accepting(ret, new_size-1, true);
