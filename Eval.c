c21882852c347aa09803f4dd9d8b831667a105f4
Ben Kuehnert <bkuehnert@gmail.com>
2018-10-26 13:47:51 -0400

    removed uneeded print statements

--- Eval.c
+++ Eval.c
@@ -1,13 +1,8 @@
 
 #include "Eval.h"
 
-NFA buildNFA(Node root) {
-	
-}
-
 NFA build_R(Node root) {
 	if(root->children[1]->children[0]->c != 'e') {
-		printf("building UNION\n");
 		return create_Union(build_A(root->children[0]), build_R(root->children[1]->children[0]));
 	}
 	else {
@@ -17,7 +12,6 @@ NFA build_R(Node root) {
 
 NFA build_A(Node root) {
 	if(root->children[1]->children[0]->c != 'e') {
-		printf("building CONCAT\n");
 		return create_Concat(build_C(root->children[0]),build_A(root->children[1]->children[0]));
 	}
 	else {
